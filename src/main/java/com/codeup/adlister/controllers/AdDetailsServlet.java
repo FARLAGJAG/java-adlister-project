92caaf291a94d5b108f100b4611f053ac7c4fff1
aharownjackson <aharownmjackson@gmail.com>
2023-03-21 10:53:35 -0500

    saving work on the details servlet

--- src/main/java/com/codeup/adlister/controllers/AdDetailsServlet.java
+++ src/main/java/com/codeup/adlister/controllers/AdDetailsServlet.java
@@ -1,11 +1,19 @@
 package com.codeup.adlister.controllers;
 
+import com.codeup.adlister.dao.Config;
+import com.codeup.adlister.dao.DaoFactory;
+import com.codeup.adlister.dao.MySQLAdsDao;
+import com.mysql.jdbc.Driver;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.sql.*;
+
 import java.io.IOException;
+import java.sql.ResultSet;
 
 @WebServlet("/details")
 public class AdDetailsServlet extends HttpServlet {
