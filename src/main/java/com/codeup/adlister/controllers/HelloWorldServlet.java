d4d8936718e5362b0b7db2256c907d1c56a51cd0
Jona Gonzalez <jona.a.gonzalez104@gmail.com>
2023-03-21 09:28:25 -0500

    Updates Srvlets to connect to lnding page

--- src/main/java/com/codeup/adlister/controllers/HelloWorldServlet.java
+++ src/main/java/com/codeup/adlister/controllers/HelloWorldServlet.java
@@ -1,5 +1,8 @@
 package com.codeup.adlister.controllers;
 
+import com.codeup.adlister.dao.DaoFactory;
+
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -8,7 +11,7 @@ import java.io.IOException;
 
 @WebServlet(name = "controllers.HelloWorldServlet", urlPatterns = "/")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.getWriter().println("<h1>Hello, World!</h1>");
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        request.getRequestDispatcher("/index.jsp").forward(request, response);
     }
 }
